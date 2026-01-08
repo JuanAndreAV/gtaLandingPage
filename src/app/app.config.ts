4a8c1637819940e18d2aaac5a0f83d10fcaca9bf
Juan Andres Alvarez <juanandres1029@gmail.com>
2026-01-08 18:54:57 -0500

    Add auth.interceptor.ts file

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,10 +5,14 @@ import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 
 
 import { routes } from './app.routes';
-import { provideHttpClient, withJsonpSupport } from '@angular/common/http';
+import { provideHttpClient, withJsonpSupport, withInterceptors} from '@angular/common/http';
+import { logginInterceptor } from './shared/interceptors/loggin.interceptor';
+import { authInterceptor } from './auth/interceptor/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(withJsonpSupport()),
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(withJsonpSupport(),
+  withInterceptors([logginInterceptor, authInterceptor])
+  ),
   provideCharts(withDefaultRegisterables())
   ]
 };
--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -3,7 +3,8 @@ import { computed, inject, Injectable, signal,  } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { UserResponse } from '../interfaces/auth-response';
 import { AuthResponse } from '../interfaces/auth-response';
-import { Observable, tap, map, catchError, of} from 'rxjs';
+import { Observable, tap, map, catchError, of } from 'rxjs';
+import { rxResource } from '@angular/core/rxjs-interop';
 
 type AuthStatus = 'checking' | 'authenticated' | 'unauthenticated';
 const baseUrl = environment.baseUrl;
@@ -14,10 +15,16 @@ const baseUrl = environment.baseUrl;
 })
 export class AuthService {
 private _authStatus = signal<AuthStatus>('checking');
-private _token = signal<string | null>(null);
+private _token = signal<string | null>(localStorage.getItem('token'));
 private _user = signal<UserResponse | null>(null); //crear interfaz usuario
 
 private http = inject(HttpClient);
+
+// checkStatusResource = rxResource({
+  
+//   loader: ()=> this.checkStatus()
+// });
+
 authStatus = computed<AuthStatus>(()=> {
   if(this._authStatus() === 'checking')return 'checking';
   if(this._user())return 'authenticated';
@@ -26,23 +33,64 @@ authStatus = computed<AuthStatus>(()=> {
  user = computed<UserResponse | null>(()=> this._user());
  token = computed<string | null>(()=> this._token());
 
- login(email: string, password: string): Observable<boolean>{
+ login(email: string, password: string): Observable<boolean>{ // para el manejo de excepciones
   return this.http.post<AuthResponse>(`${baseUrl}/auth/login`, {email, password}).pipe(
-    tap(response => {
-      this._authStatus.set('authenticated');
+    tap(response => this.handleAuthSuccess(response)
+    //   {
+    //   this._authStatus.set('authenticated');
+    //   this._token.set(response.access_token);
+    //   this._user.set(response.user);
+
+    //   localStorage.setItem('token', response.access_token);
+    // }
+  ),
+    map(()=> true),//al usar el map devolvemos un booleano
+    catchError((error: any) => this.handleAuthError(error)
+    //    {
+    //   this._authStatus.set('unauthenticated');
+    //   this._token.set(null);
+    //   this._user.set(null);
+    //   localStorage.removeItem('token');
+    //   return of(false);
+    // }
+  )
+  );
+ }
+  checkStatus(): Observable<boolean>{
+    const token =localStorage.getItem('token');
+    if(!token){
+      //this._authStatus.set('unauthenticated');
+      this.logout()
+      return of(false);
+    }
+    return this.http.get<AuthResponse>(`${baseUrl}/auth/check-token`,{
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    }).pipe(
+    tap(response => this.handleAuthSuccess(response)),
+    map(()=> true),//al usar el map devolvemos un booleano
+    catchError((error: any) => this.handleAuthError(error))
+  );
+  }
+  logout(){
+    this._authStatus.set('unauthenticated');
+    this._token.set(null);
+    this._user.set(null);
+    localStorage.removeItem('token');
+   }
+
+   private handleAuthSuccess(response: AuthResponse){
+    this._authStatus.set('authenticated');
       this._token.set(response.access_token);
       this._user.set(response.user);
 
       localStorage.setItem('token', response.access_token);
-    }),
-    map(()=> true),//al usar el map devolvemos un booleano
-    catchError((error: any) => {
-      this._authStatus.set('unauthenticated');
-      this._token.set(null);
-      this._user.set(null);
-      localStorage.removeItem('token');
-      return of(false);
-    })
-  );
- }
+   }
+   private handleAuthError(error: any){
+    this.logout();
+    return of(false)
+   }
 }
+
+
