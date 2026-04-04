d866fa727127f654ef6252231566a93e88dfee73
Juan Andres Alvarez <juanandres1029@gmail.com>
2026-04-03 21:10:15 -0500

    chore: dejar de rastrear archivos de entorno

--- src/environments/environment.development.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-    baseUrl: 'http://localhost:3002/api',
-    q10Api: 'https://api.q10.com/v1',
-    q10ApiKey: '7ef3a3164d1c47ebbbd',
-    culturaEventosApi: 'http://localhost:3001/api/v1',
-    production: false
-};
--- src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-    baseUrl: 'http://localhost:3002/api',
-    q10Api: 'https://api.q10.com/v1',
-    q10ApiKey: '7ef3a3164d1c47',
-    culturaEventosApi: 'https://cultura-nest-api-',
-    production: true
-    
-};
