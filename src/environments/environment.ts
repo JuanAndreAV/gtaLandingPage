348e2666c4226de5cea0ebe537047471204af301
Juan Andres Alvarez <juanandres1029@gmail.com>
2026-04-03 21:05:00 -0500

    environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
     baseUrl: 'http://localhost:3002/api',
     q10Api: 'https://api.q10.com/v1',
-    q10ApiKey: '7ef3a3164d1c47ebbbd681de99c20e57',
-    culturaEventosApi: 'https://cultura-nest-api--giaradotaescultura.us-central1.hosted.app/api/v1',
+    q10ApiKey: '7ef3a3164d1c47',
+    culturaEventosApi: 'https://cultura-nest-api-',
     production: true
     
 };
